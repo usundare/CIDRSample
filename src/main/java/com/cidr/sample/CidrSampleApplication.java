dc0945e01010cf74e9bbad06effaee1a094ce22c
usundare <usundare@gmail.com>
2019-09-20 18:14:04 -0400

    Add files via upload

---
+++ src/main/java/com/cidr/sample/CidrSampleApplication.java
@@ -0,0 +1,13 @@
+package com.cidr.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidrSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidrSampleApplication.class, args);
+	}
+
+}
