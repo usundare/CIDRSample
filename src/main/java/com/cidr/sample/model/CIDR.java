2997cad4b3498425078472c08df72bbca378c554
usundare <usundare@gmail.com>
2019-09-20 18:19:11 -0400

    Add files via upload

---
+++ src/main/java/com/cidr/sample/model/CIDR.java
@@ -0,0 +1,20 @@
+package com.cidr.sample.model;
+
+public class CIDR {
+	
+	private int id;
+	private String cidr;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCidr() {
+		return cidr;
+	}
+	public void setCidr(String cidr) {
+		this.cidr = cidr;
+	}
+
+}
